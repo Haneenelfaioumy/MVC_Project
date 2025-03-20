423ba6e99fef76e8ffc53694759414ec345b70a2
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-03-20 06:38:43 +0200

    Part 03 Department Module [ Entities & Configrations] and DbContext

---
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Reflection;
+
+namespace Demo.DataAccess.Data.Contexts
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("ConnectionString");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
