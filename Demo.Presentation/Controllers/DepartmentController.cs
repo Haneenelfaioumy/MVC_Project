b5068710c28cc4d9cc235e073b72809b636b2326
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-03-21 21:09:40 +0200

    Part 02 Migrate Database

--- Demo.Presentation/Controllers/DepartmentController.cs
+++ Demo.Presentation/Controllers/DepartmentController.cs
@@ -5,7 +5,12 @@ namespace Demo.Presentation.Controllers
 {
     public class DepartmentController(IDepartmentService departmentServices) : Controller
     {
-        
+        //private readonly IDepartmentService _departmentServices;
+        //public DepartmentController(IDepartmentService departmentServices)
+        //{
+        //    this._departmentServices = departmentServices;
+        //}
+
         public IActionResult Index()
         {
             var Departments = departmentServices.GetAllDepartments();
