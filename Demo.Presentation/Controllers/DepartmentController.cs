b655d6746aea5f7ef805cf697283662ad9da9a56
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-03-22 02:29:04 +0200

    Part 03 Department Controller Index

--- Demo.Presentation/Controllers/DepartmentController.cs
+++ Demo.Presentation/Controllers/DepartmentController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presentation.Controllers
 {
-    public class DepartmentController(IDepartmentService departmentServices) : Controller
+    public class DepartmentController(IDepartmentService _departmentService) : Controller
     {
         //private readonly IDepartmentService _departmentServices;
         //public DepartmentController(IDepartmentService departmentServices)
@@ -11,11 +11,12 @@ namespace Demo.Presentation.Controllers
         //    this._departmentServices = departmentServices;
         //}
 
+        // GET BaseUrl/Department/Index
+        [HttpGet]
         public IActionResult Index()
         {
-            var Departments = departmentServices.GetAllDepartments();
-
-            return View();
+            var departments = _departmentService.GetAllDepartments();
+            return View(departments);
         }
     }
 }
