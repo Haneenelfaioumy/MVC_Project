b1fe5401743049e981ddfc6f6faa3638b56c90b2
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-03-22 16:06:56 +0200

    Part 04 Department Controller Create

--- Demo.BusinessLogic/DataTransferObjects/CreatedDepartmentDto.cs
+++ Demo.BusinessLogic/DataTransferObjects/CreatedDepartmentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace Demo.BusinessLogic.DataTransferObjects
 {
     public class CreatedDepartmentDto
     {
+        [Required (ErrorMessage = "Name Is Required !!!!!")]
         public string Name { get; set; } = null!;
+        [Required]
+        [Range(100 , int.MaxValue)]
         public string Code { get; set; } = null!;
-        public DateOnly DateOfCreation { get; set; }
         public string? Description { get; set; }
-
+        public DateOnly DateOfCreation { get; set; }
     }
 }
