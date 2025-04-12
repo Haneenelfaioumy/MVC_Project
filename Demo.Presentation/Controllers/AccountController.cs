c77e31f793d7853eb9044b160490895114d0c45e
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-04-12 07:01:09 +0200

    Part 07 AuthLayout

---
+++ Demo.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Register
+        public IActionResult Register() => View();
+
+        // Login 
+
+        //Sign Out
+    }
+}
